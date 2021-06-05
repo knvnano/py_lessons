c180802a9185882647d796cee63cbe9289550680
knv_nano <knv_nano@bk.ru>
2021-06-05 17:43:22 +0300

    Add main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+def get_vat(payment, percent=20):
+    try:
+        payment = float(payment)
+        vat = payment / 100 * percent
+        vat = round(vat, 2)
+        return "Sum VAT: {}".format(vat)
+    except TypeError:
+        return "CANT COUNT"
+
+
+result = get_vat(400, 12)
+
+print(result)
